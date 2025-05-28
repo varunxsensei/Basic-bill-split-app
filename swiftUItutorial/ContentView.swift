cb0821a6786d434dbb9f9bf5e0672a0d29f01312
varunxsensei <varunxai27@gmail.com>
2025-05-28 19:16:01 +0530

    Initial Commit

---
+++ swiftUItutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUItutorial
+//
+//  Created by Varun Saxena on 28/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUItutorial/swiftUItutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUItutorialApp.swift
+//  swiftUItutorial
+//
+//  Created by Varun Saxena on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUItutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
